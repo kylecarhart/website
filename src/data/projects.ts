a0ba53f15ca6205d1ddf4d251d2bf16d0f87772a
Kyle Carhart <77036988+kylecarhart@users.noreply.github.com>
2025-01-26 17:41:15 -0500

    fix up buttons

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -11,16 +11,16 @@ export type Project = {
 
 const projects = {
   runner: {
-    title: "Project Runner",
+    title: "Project: Runner",
     description:
-      "All-in-one platform for running enthusiasts to sign up for events, track stats, and compete with others!",
+      "All-in-one platform for running enthusiasts to sign up for events, track stats, and compete with others.",
     tech: [tech.astro, tech.ts, tech.react, tech.hono, tech.cfw, tech.cfp],
     url: "https://github.com/kylecarhart/Runner",
   },
   domainFilter: {
     title: "Domain Filter for Google™ Search",
     description:
-      "Cross platform web extension enabling users to filter domains from search results.",
+      "Cross platform web extension enabling users to filter domains from Google™ search results.",
     tech: [tech.ts, tech.react, tech.redux, tech.webpack, tech.webExt],
     url: "https://github.com/kylecarhart/Google-Domain-Filter",
   },
